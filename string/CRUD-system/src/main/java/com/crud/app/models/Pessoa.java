1b3d8f707d58e70214911b04dfbf1809e2c8468d
marcuspSousa <cursosbrasiliaead@gmail.com>
2025-09-11 17:42:23 -0300

    aula atividade 11/09/2025

---
+++ string/CRUD-system/src/main/java/com/crud/app/models/Pessoa.java
@@ -0,0 +1,60 @@
+package com.crud.app.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long idpessoa;
+    private String nome;
+    private String  cpf;
+    private String email; 
+
+    public Pessoa(long idpessoa, String nome, String cpf, String email) {
+        this.idpessoa = idpessoa;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public long getIdpessoa() {
+        return this.idpessoa;
+    }
+
+    public void setIdpessoa(long idpessoa) {
+        this.idpessoa = idpessoa;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
