b91c194c02df4dc44c9e63f513f3311892d0814c
marcuspSousa <cursosbrasiliaead@gmail.com>
2025-09-16 17:36:06 -0300

    aula atividade 16/09/2025

---
+++ string/CRUD-system/src/main/java/com/crud/app/controllers/CrudController.java
@@ -0,0 +1,42 @@
+package com.crud.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.crud.app.models.Pessoa;
+import com.crud.app.repository.AppRepository;
+
+
+
+@Controller
+public class CrudController {
+    @Autowired
+    private AppRepository csr;
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+    @RequestMapping(value = "/cadastrar", method = RequestMethod.GET)
+    public String cadastrar() {
+        return "cadastrar";
+    }
+
+    @RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
+    public String cadastrar(Pessoa usuario){
+        csr.save(usuario);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/listar")
+    public ModelAndView listar() {
+        ModelAndView mv = new ModelAndView("listar");
+        Iterable<Pessoa> usuarios = csr.findAll();
+        mv.addObject("usuarios", usuarios);
+        return mv;
+    }
+
+}
